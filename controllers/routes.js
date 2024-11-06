de5479864a32b906976c4a472c3d763d40306dd7
perla244 <lunajanethc45@gmail.com>
2024-11-06 09:50:21 -0700

    rutas eliminadas

--- controllers/routes.js
+++ controllers/routes.js
@@ -9,7 +9,6 @@ router.get("/perfilAdmin", (req, res) => {
     res.render("perfilAdmin");
 });
 
-
 router.get("/ActualizarContraseña", (req, res) => {
     res.render("actualizar_contraseña");
 });
@@ -18,76 +17,28 @@ router.get("/Alumnos", (req, res) => {
     res.render("alumnos");
 });
 
-router.get("/Becas", (req, res) => {
-    res.render("Becas");
-});
-
 router.get("/BuzonQuejas", (req, res) => {
     res.render("BuzonQuejas");
 });
 
-router.get("/Carreras", (req, res) => {
-    res.render("Carreras");
-});
-
-/*router.get("/Chat", (req, res) => {
-    res.render("Chat",{datos:[]});
-});*/
-
 router.get("/Contacto", (req, res) => {
     res.render("contacto");
-});
-
-router.get("/ExamenAdmision", (req, res) => {
-    res.render("Examen");
 });
 
 router.get("/Login", (req, res) => {
     res.render("login");
 });
 
-router.get("/MapaDelSitio", (req, res) => {
-    res.render("mapa");
-});
-
 router.get("/Recuperar", (req, res) => {
     res.render("RecuperarContra");
-});
-
-router.get("/Recuperar2", (req, res) => {
-    res.render("RecuperarMensaje");
-});
-
-router.get("/Recuperar3", (req, res) => {
-    res.render("RecuperarPregunta");
-});
-
-router.get("/Recuperar4", (req, res) => {
-    res.render("VerificacionCelular");
 });
 
 router.get("/Registro", (req, res) => {
     res.render("registro");
 });
 
-router.get("/SobreNosotros", (req, res) => {
-    res.render("SobreNosotros");
-});
-
 router.get("/Perfil", (req, res) => {
     res.render("Perfil");
-});
-
-router.get("/404", (req, res) => {
-    res.render("404");
-});
-
-router.get("/faqs", (req, res) => {
-    res.render("faqs");
-});
-
-router.get("/calificaciones", (req, res) => {
-    res.render("calificaciones");
 });
 
 module.exports = router;
