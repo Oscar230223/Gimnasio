3e9fe44b5f04be8765579f879542caf2e88af203
perla244 <lunajanethc45@gmail.com>
2024-11-05 23:39:47 -0700

    ajustes de diseño (registro, index, contacto, buzon, recuperar)

--- src/public/js/Footer.js
+++ src/public/js/Footer.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const dynamicFooter = document.getElementById("dynamic-footer");
     /*const message = `✆ <a href="contacto.html">Contáctanos</a> - 656 782 5559\n📍 Blvd. Independencia #2810. H Col. Praderas del Bravo 32695 Ciudad Juárez Chihuahua México\n© Contenido exclusivo de la Universidad Fronteriza de Ciudad Juárez`*/
     const message = `✆ <a href="contacto" style="color: white">Contáctanos - 656 782 5559</a>
-        📍 Blvd. Independencia 1510, 32599 Juárez, Chih
-        © Contenido exclusivo y propiedad de EmergencyApp
+         <a href="/maps" style="color: white">📍 Blvd. Independencia 1510, 32599 Juárez, Chih.</a>
+        © Contenido exclusivo y propiedad de EmergencyApp 
     `;
     dynamicFooter.innerHTML = message;
 });
